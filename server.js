907d65cd716fbfba937bd99f1369898f231c2801
yacineops <37175147+yacineops@users.noreply.github.com>
2026-09-13 01:24:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-
+{
+  "name": "battleship-game",
+  "version": "1.0.0",
+  "description": "Online Battleship Game",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "ws": "^8.18.0"
+  }
+}
